8a087a8427dd52412e703cb034a9a0e04dc05393
kaining <xkn1994@gmail.cin>
2021-03-16 11:53:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Animesite
+# MyAnime
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.4.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Animesite'`, () => {
+  it(`should have as title 'MyAnime'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Animesite');
+    expect(app.title).toEqual('MyAnime');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Animesite app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MyAnime app is running!');
   });
 });
