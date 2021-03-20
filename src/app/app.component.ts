b9a3c6c5d57c9b530249059ad21031201f13b86e
kaining <xkn1994@gmail.cin>
2021-03-20 18:27:05 +0100

    v0.1 search by name done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaChange, MediaObserver} from '@angular/flex-layout';
+import {Subscription} from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyAnime';
+export class AppComponent implements OnInit, OnDestroy {
+  mediaSub: Subscription;
+  deviceXs: boolean;
+  constructor(public mediaObserver: MediaObserver) {
+  }
+
+  ngOnInit(): void {
+    // get media device type
+    this.mediaSub = this.mediaObserver.media$.subscribe((result: MediaChange) => {
+      console.log(result.mqAlias);
+      this.deviceXs = result.mqAlias === 'xs';
+      }
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    this.mediaSub.unsubscribe();
+  }
+
+
 }
