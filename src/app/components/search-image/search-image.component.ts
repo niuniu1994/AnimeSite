f889e469b98ff734fe0007708cc142efce40f3fd
kaining <xkn1994@gmail.cin>
2021-03-21 20:48:13 +0100

    v0.1 anime image search

---
+++ src/app/components/search-image/search-image.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {Idoc} from '../entity/ianime-image-response';
+import {AnimeService} from '../../services/anime.service';
+
+@Component({
+  selector: 'app-search-image',
+  templateUrl: './search-image.component.html',
+  styleUrls: ['./search-image.component.css']
+})
+export class SearchImageComponent implements OnInit {
+  public imageUrl: any;
+  public alert: string;
+  public animeName: string;
+  public animes: Idoc[];
+  public loading = false;
+
+  constructor(private animeService: AnimeService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  private include(allowedTypes: string[], type: string): boolean {
+    for (const x of allowedTypes) {
+      if (x === type) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  uploadFile(event): void {
+    const reader = new FileReader(); // HTML5 FileReader API
+    if (event.target.files && event.target.files[0]) {
+      const allowedTypes = ['image/png', 'image/jpeg'];
+      const maxSize = 1_048_576 * 10;
+      const file = event.target.files[0];
+      if (event.target.files[0].size > maxSize) {
+        this.alert = 'Maximum size allowed is 10 Mb';
+        return;
+      }
+      if (!this.include(allowedTypes, event.target.files[0].type)) {
+        this.alert = 'Only Images are allowed ( JPEG | PNG )';
+        return;
+      }
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log(reader.result);
+        this.imageUrl = reader.result;
+      };
+    }
+  }
+
+  search(): void {
+    this.loading = true;
+    this.animeService.searchAnimeImage(this.imageUrl).subscribe(
+      x => {
+        this.animes = x.docs;
+        this.loading = false;
+      }
+    )
+    ;
+  }
+
+
+}
