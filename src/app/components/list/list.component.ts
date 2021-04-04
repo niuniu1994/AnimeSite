f496db8b4bc3c2d282882b7ef48c8637f4ac2860
kaining <xkn1994@gmail.cin>
2021-04-04 20:59:38 +0200

    genre modification

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -12,6 +12,10 @@ import {Subscription} from 'rxjs';
 export class ListComponent implements OnInit, OnDestroy {
   public searchText: string;
   public params: string;
+
+  public genre: string;
+  public page = '1';
+
   public animes: IAnime[] = [];
   private subActivetedRoute: Subscription;
   private subAnimes: Subscription;
@@ -25,20 +29,34 @@ export class ListComponent implements OnInit, OnDestroy {
     this.subActivetedRoute = this.activatedRoute.paramMap.subscribe(x => {
         this.searchText = x.get('searchText');
         this.params = x.get('params');
+        this.genre = x.get('genre');
       }
     );
-    console.log(this.searchText + ' ' + this.params);
-    this.subAnimes = this.animeService.searchAnimeByText(this.params, this.searchText).subscribe(z => {
-      this.animes = z.results;
-      console.log(this.animes);
-    });
+
+    if (this.genre != null) {
+      this.subAnimes = this.animeService.searchAnimeByGenre(this.genre, this.page).subscribe(z => {
+        this.animes = z.anime;
+      });
+    } else {
+      this.subAnimes = this.animeService.searchAnimeByText(this.params, this.searchText).subscribe(z => {
+        this.animes = z.results;
+      });
+    }
+
   }
 
   pageChanged(evt: number): void {
-    this.params = `&page=${evt}`;
-    this.animeService.searchAnimeByText(this.params, this.searchText).subscribe(z => {
-      this.animes = z.results;
-    });
+    if (this.genre != null) {
+      this.page = `${evt}`;
+      this.animeService.searchAnimeByGenre(this.params, this.page).subscribe(z => {
+        this.animes = z.anime;
+      });
+    } else {
+      this.params += `&page=${evt}`;
+      this.animeService.searchAnimeByText(this.params, this.searchText).subscribe(z => {
+        this.animes = z.results;
+      });
+    }
     this.p = evt;
   }
 
