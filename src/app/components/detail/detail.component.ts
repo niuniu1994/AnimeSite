399841408ba17d4a6e9b376192fde3c6580ea303
Tyler <tyler.boyeka@efrei.net>
2021-04-04 17:10:29 +0200

    Précision avec écrit N/A lorsque les données ne sont pas disponible

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,31 +1,61 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AnimeService} from '../../services/anime.service';
 import {IAnimeDetailResponse} from '../../entity/ianime-detail-response';
-import {Subscription} from 'rxjs';
+import {tryCatch} from 'rxjs/internal-compatibility';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class DetailComponent implements OnInit, OnDestroy {
+export class DetailComponent implements OnInit {
   public id: string;
+  public videoId: string;
   public anime: IAnimeDetailResponse;
-  private subActiveRoute: Subscription;
-
+  dataEmpty = 'N/A';
   constructor(private activatedRoute: ActivatedRoute, private animeService: AnimeService) {
   }
 
   ngOnInit(): void {
-    this.subActiveRoute = this.activatedRoute.paramMap.subscribe(x => this.id = x.get('id'));
+    this.activatedRoute.paramMap.subscribe(x => this.id = x.get('id'));
     console.log(this.id);
     this.animeService.getAnimeDetail(this.id).subscribe(x => this.anime = x);
   }
 
-  ngOnDestroy(): void {
-    this.subActiveRoute.unsubscribe();
+  // tslint:disable-next-line:typedef
+  getScore() {
+    const min = 'a';
+    try{
+      if (!(min.length > this.anime.score.length)){
+        return this.anime.score + ' / 10';
+      }
+    }
+    catch (e) {
+      return this.dataEmpty;
+    }
   }
 
-
+  // tslint:disable-next-line:typedef
+  getPremiered() {
+    try{
+      if (this.anime.premiered.length > 1){
+        return this.anime.premiered;
+      }
+    }
+    catch (e){
+      return this.dataEmpty;
+    }
+  }
+  // tslint:disable-next-line:typedef
+  getBroadcast() {
+    try{
+      if (this.anime.broadcast.length > 1){
+        return this.anime.broadcast;
+      }
+    }
+    catch (e){
+      return this.dataEmpty;
+    }
+  }
 }
