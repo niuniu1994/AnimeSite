dbe36ceaf028b3114d14e145f5cca8cf0a186573
kaining <xkn1994@gmail.cin>
2021-04-05 12:26:00 +0200

    corrections

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/my-anime'));
+app.use(express.static('./dist/MyAnime'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/my-anime/'}
+  res.sendFile('index.html', {root: 'dist/MyAnime'}
 );
 });
 app.listen(process.env.PORT || 8080);
