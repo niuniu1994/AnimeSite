449a887f9a9e80849375322f04be58820c499457
kaining <xkn1994@gmail.cin>
2021-04-05 11:47:53 +0200

    configs for deploy on heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/my-anime'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/my-anime/'}
+);
+});
+app.listen(process.env.PORT || 8011);
